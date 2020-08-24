0ceefbecd0ee48968636eb9c0c37366cf9143224
serlus <serluscasas@gmail.com>
2020-08-24 13:41:34 -0300

    create app - close #5

---
+++ simpleproject/base/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+# Tem a função básica de responder as requisoções q nós fazemos pelo navegador
+
+
+def home(request):  # sempre receberá o request
+    return HttpResponse('Olá Django')
